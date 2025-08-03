e728c11f6b9b0dadf0bd07700560ad3e52f8ba70
umurakbulut <akbulutumur@gmail.com>
2025-08-03 16:50:22 +0300

    fix: hydration mismatch

--- src/components/CountrySelect.tsx
+++ src/components/CountrySelect.tsx
@@ -4,6 +4,7 @@ import Select from "react-select";
 import { countries } from "@/constants/country";
 import { getFlagEmoji } from "@/utils/getFlagEmoji";
 import { ICountry } from "@/types/country";
+import useHasMounted from "@/hooks/useHasMounted";
 
 interface ICountrySelectProps {
   value: ICountry["code"];
@@ -14,6 +15,8 @@ export default function CountrySelect({
   value,
   onChange,
 }: ICountrySelectProps) {
+  const hasMounted = useHasMounted();
+
   const options = countries.map((country) => ({
     value: country.code,
     label: `${getFlagEmoji(country.code)} ${country.name} (${
@@ -26,16 +29,18 @@ export default function CountrySelect({
   return (
     <div className="space-y-1">
       <label className="block text-sm font-medium">Country</label>
-      <Select
-        options={options}
-        value={selectedOption}
-        onChange={(selected) => {
-          if (selected) onChange(selected.value);
-        }}
-        isSearchable
-        className="react-select-container"
-        classNamePrefix="react-select"
-      />
+      {hasMounted && (
+        <Select
+          options={options}
+          value={selectedOption}
+          onChange={(selected) => {
+            if (selected) onChange(selected.value);
+          }}
+          isSearchable
+          className="react-select-container"
+          classNamePrefix="react-select"
+        />
+      )}
     </div>
   );
 }
--- src/components/FormatSelect.tsx
+++ src/components/FormatSelect.tsx
@@ -3,6 +3,7 @@
 import Select from "react-select";
 import { phoneFormatOptions } from "@/constants/phone";
 import { PhoneFormatType } from "@/types/phone";
+import useHasMounted from "@/hooks/useHasMounted";
 
 interface IFormatSelectProps {
   value: PhoneFormatType;
@@ -10,18 +11,20 @@ interface IFormatSelectProps {
 }
 
 export default function FormatSelect({ value, onChange }: IFormatSelectProps) {
+  const hasMounted = useHasMounted();
+
   return (
     <div>
       <label className="block text-sm font-medium mb-1">Format Type</label>
-      <Select
-        options={phoneFormatOptions}
-        value={phoneFormatOptions.find((o) => o.value === value)}
-        onChange={(selected) => {
-          if (selected) onChange(selected.value as PhoneFormatType);
-        }}
-        className="react-select-container"
-        classNamePrefix="react-select"
-      />
+      {hasMounted && (
+        <Select
+          options={phoneFormatOptions}
+          value={phoneFormatOptions.find((o) => o.value === value)}
+          onChange={(selected) => {
+            if (selected) onChange(selected.value as PhoneFormatType);
+          }}
+        />
+      )}
     </div>
   );
 }
